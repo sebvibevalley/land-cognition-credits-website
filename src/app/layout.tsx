d34a9701953d80d6288795d2d466ce08bfd4cb0e
sebvibevalley <seb@vibevalley.io>
2026-06-08 20:01:47 +0200

    Update layout.tsx to import globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 import Providers from './providers';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: {
